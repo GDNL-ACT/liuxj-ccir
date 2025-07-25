1dd526105648cbc8d20eff0bdd29a3f02a834ccb
liuxj25 <21371243@buaa.edu.cn>
2025-07-25 07:43:28 +0000

    init

--- finetune.sh
+++ finetune.sh
@@ -1,5 +1,12 @@
 #!/bin/zsh
 
+
+# 安装LLaMAFactory
+git clone --depth 1 https://github.com/hiyouga/LLaMA-Factory.git
+cd LLaMA-Factory
+pip install -e ".[torch,metrics]" --no-build-isolation
+
+# 训练运行脚本
 cd /home/liuxj25/LawLLM/LLaMA-Factory
 
 FORCE_TORCHRUN=1 llamafactory-cli train ccir/qwen3-32b-lora-sft-ds3.yaml 
